fefc6c30ca0359a7d4e466135f4ef68eeac65a54
Tilok Saha <tiloksaha60@gmail.com>
2023-02-22 17:35:18 +0530

    designed new boss area

--- globals/singletons/CameraPosition.gd
+++ globals/singletons/CameraPosition.gd
@@ -6,6 +6,7 @@ onready var tween = $Tween
 
 export var right_border_limit:int = 268
 export var bottom_border_limit:int = 166
+export var left_border_limit:int = 444
 export var length_of_offset:float = 4
 
 
@@ -19,7 +20,7 @@ func _process(_delta):
 
 
 func _get_position():
-	var camera_position_x = clamp(player_position.position.x - 128, -INF, right_border_limit-256)
+	var camera_position_x = clamp(player_position.position.x - 128, -left_border_limit, right_border_limit-256)
 	var camera_position_y = clamp(player_position.position.y - 75, -INF, bottom_border_limit-150)
 	
 	return Vector2(camera_position_x, camera_position_y)
